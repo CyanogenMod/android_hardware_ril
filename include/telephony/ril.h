e97b82550ebb6c8f71d169a65942657857193081
Naveen Kalla <nkalla@codeaurora.org>
2011-11-15 12:51:42 -0800

    Add RIL_APPSTATE_ILLEGAL required by qcril
    Change-Id: Ia87c1420f3801429c4b0789c1413eac8c1663c9f

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -475,6 +475,7 @@ typedef enum {
 } RIL_PersoSubstate;
 
 typedef enum {
+    RIL_APPSTATE_ILLEGAL               = -1,
     RIL_APPSTATE_UNKNOWN               = 0,
     RIL_APPSTATE_DETECTED              = 1,
     RIL_APPSTATE_PIN                   = 2, /* If PIN1 or UPin is required */
