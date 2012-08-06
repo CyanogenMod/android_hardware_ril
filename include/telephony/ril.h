4cba4f8de98f89c726aff7aeb31c55607598a507
benjamingwynn <benjamin@gwynn.tk>
2012-08-06 09:10:02 -0700

    Lowered minimum version number for RIL.
    This allows developers to use older RIL versions. This fixes telephony on some devices (mainly HTC ones) as they only have the option to use an old RIL. This doesn't impact newer versions of RIL either.
    
    Change-Id: Id68cd3f0ee500331fc39f2ed5c79d11f547cd19e

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -27,7 +27,7 @@ extern "C" {
 #endif
 
 #define RIL_VERSION 7     /* Current version */
-#define RIL_VERSION_MIN 6 /* Minimum RIL_VERSION supported */
+#define RIL_VERSION_MIN 2 /* Minimum RIL_VERSION supported */
 
 #define CDMA_ALPHA_INFO_BUFFER_LENGTH 64
 #define CDMA_NUMBER_INFO_BUFFER_LENGTH 81
