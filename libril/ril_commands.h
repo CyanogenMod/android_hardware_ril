a6b5d1473d687b16b4ac17ca569d9ca255096a4c
Emerson Pinter <dev@pinter.com.br>
2013-07-29 23:57:01 -0300

    libril: some blobs don't support CELL_INFO_LIST commands
    RIL blobs that don't support CELL_INFO_LIST needs this ifndefs,
    or rild keeps crashing with segfaults.
    
    Change-Id: Ifcd717740db812ce9a2371fcb20ed61d07efffe1

--- libril/ril_commands.h
+++ libril/ril_commands.h
@@ -125,8 +125,10 @@
     {RIL_REQUEST_ACKNOWLEDGE_INCOMING_GSM_SMS_WITH_PDU, dispatchStrings, responseVoid},
     {RIL_REQUEST_STK_SEND_ENVELOPE_WITH_STATUS, dispatchString, responseSIM_IO},
     {RIL_REQUEST_VOICE_RADIO_TECH, dispatchVoiceRadioTech, responseInts},
+#ifndef RIL_NO_CELL_INFO_LIST
     {RIL_REQUEST_GET_CELL_INFO_LIST, dispatchVoid, responseCellInfoList},
     {RIL_REQUEST_SET_UNSOL_CELL_INFO_LIST_RATE, dispatchInts, responseVoid},
+#endif
     {RIL_REQUEST_SET_INITIAL_ATTACH_APN, dispatchSetInitialAttachApn, responseVoid},
     {RIL_REQUEST_IMS_REGISTRATION_STATE, dispatchVoid, responseInts},
     {RIL_REQUEST_IMS_SEND_SMS, dispatchImsSms, responseSMS},
