e3085f770de6e802aba09946b18547d70c054e9e
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-23 20:30:19 -0500

    ril: Allow devices to opt-out of building rild
    Change-Id: I0e986018dccb53fdcb695c85b54776889c2f8ddc

--- rild/Android.mk
+++ rild/Android.mk
@@ -1,5 +1,7 @@
 # Copyright 2006 The Android Open Source Project
 
+ifneq ($(BOARD_PROVIDES_RILD),true)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -48,3 +50,5 @@ LOCAL_MODULE:= radiooptions
 LOCAL_MODULE_TAGS := debug
 
 include $(BUILD_EXECUTABLE)
+
+endif
