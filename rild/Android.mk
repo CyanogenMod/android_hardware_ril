eb67a6f599afc1a85e541ba7b5bd58ea10dd6f7b
Steve Kondik <steve@cyngn.com>
2016-06-24 12:21:12 -1000

    ril-caf: Fix build when using prebuilt libril
     * ril-caf was failing to compile when a prebuilt libril was
       in use. This is obviously not an optimal configuration, but
       is necessary in a couple of cases. Hardcode the include
       path so ril_ex.h gets picked up.
    
    Change-Id: I35221b306662cd0a99041b4768fad51e35115b76

--- rild/Android.mk
+++ rild/Android.mk
@@ -29,6 +29,10 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/libril
 
+ifeq ($(BOARD_PROVIDES_LIBRIL),true)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
+endif
+
 include $(BUILD_EXECUTABLE)
 
 # For radiooptions binary
