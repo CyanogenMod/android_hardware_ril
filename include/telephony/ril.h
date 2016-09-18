4e9155d780471fee3c480014a4988a63a73df232
Alex Naidis <alex.naidis@linux.com>
2016-09-18 21:22:41 +0200

    rild: support to provide RIL V11 ABI if libril is prebuilt
    * fixes the case where we need RIL V11, OSS rild and prebuilt libril (v11)
    
    Change-Id: Idd369e06f15fd97927d29ad2b03cef3c8eeef8d8
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -5845,6 +5845,7 @@ struct RIL_Env {
 
     void (*RequestTimedCallback) (RIL_TimedCallback callback,
                                    void *param, const struct timeval *relativeTime);
+#if (RIL_VERSION == 12 && OSS_LIBRIL)
    /**
     * "t" is parameter passed in on previous call RIL_Notification routine
     *
@@ -5852,6 +5853,7 @@ struct RIL_Env {
     * by them and an ack needs to be sent back to java ril.
     */
     void (*OnRequestAck) (RIL_Token t);
+#endif
 };
 
 
@@ -5908,6 +5910,7 @@ void RIL_register (const RIL_RadioFunctions *callbacks);
 void RIL_onRequestComplete(RIL_Token t, RIL_Errno e,
                            void *response, size_t responselen);
 
+#if (RIL_VERSION == 12 && OSS_LIBRIL)
 /**
  * RIL_onRequestAck will be called by vendor when an Async RIL request was received by them and
  * an ack needs to be sent back to java ril. This doesn't mark the end of the command or it's
@@ -5919,6 +5922,7 @@ void RIL_onRequestComplete(RIL_Token t, RIL_Errno e,
  *          routine.
  */
 void RIL_onRequestAck(RIL_Token t);
+#endif
 
 #if defined(ANDROID_MULTI_SIM)
 /**
